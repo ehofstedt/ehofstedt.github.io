9b1a34ee96bbc4e75740893f57e45dd44350c3e1
Eleanor Hofstedt <ehofstedt@fb.com>
2021-10-25 08:25:11 -0400

    New file for homework 6a

---
+++ week7_lab/zooanimals.js
@@ -0,0 +1,16 @@
+function Koala(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = “A description of my animal”;
+    this.image = "noun_Koala_1055085.png";
+  }
+
+  function Giraffe(name, age) {
+    this.name = name;
+    this.age = age;
+    this.image_alt = “A description of my animal”;
+    this.image = src="noun_Giraffe_591427.png";
+  }
+  
+  //Comment
+  
